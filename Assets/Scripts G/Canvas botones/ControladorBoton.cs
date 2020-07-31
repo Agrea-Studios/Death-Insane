39d865e643b1ed7685cc286f430af9c26b0d1796
SrSalchicha <62081821+SrSalchicha@users.noreply.github.com>
2020-07-31 16:28:21 -0500

    animaciones y codigo
    actualizacion 31/07/2020
    Jorge Brandon uwu

---
+++ Assets/Scripts G/Canvas botones/ControladorBoton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
+public class ControladorBoton : MonoBehaviour
+{
+    public void CambiarEscena(string nombre)
+    {
+
+        print("cambiando de escena" + nombre);
+        SceneManager.LoadScene(nombre);
+    }
+
+    public void Salir()
+    {
+        print("SALIENDO DEL JUEGO");
+        Application.Quit();//tengo hambre
+
+    }
+
+   public void construir(GameObject construir)
+    {
+        for (int i = 0; i < 1; i++)
+        {
+
+            Instantiate(construir);
+        }
+    }
+
+    public void desturir(GameObject destruir)
+    {
+        Destroy(destruir);
+    }
+}
---
+++ Assets/Scripts G/Canvas botones/FixCanvasAP.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+public class FixCanvasAP : MonoBehaviour
+{
+    private Canvas ap;
+
+    private Camera Tc;
+    
+    enum RenderModeStates {camera, overlay, world}
+    RenderModeStates apR;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ap = GetComponent<Canvas>();
+        Tc = Camera.main;
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+        ap.renderMode = RenderMode.ScreenSpaceCamera;
+        apR = RenderModeStates.camera;
+        ap.worldCamera = Tc;
+    }
+}
