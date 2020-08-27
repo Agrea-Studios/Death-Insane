0c9f721d8ef8579b38ccd2ab5e19fdb60e979cdf
SrSalchicha <62081821+SrSalchicha@users.noreply.github.com>
2020-08-27 16:25:24 -0500

    actualizacion
    pruebas de movimiento

--- Assets/Codigos BD/BD Armas/ClasesA.cs
+++ Assets/Codigos BD/BD Armas/ClasesA.cs
@@ -12,11 +12,15 @@ public class ClasesA : MonoBehaviour
     public bool equipado = false;
     public float SDPS = 10f;
  }
+
+
  public class Espadabendita{
     public float ATK = 30F; //ATAQUE XDXD
     public float defensa = 5f;
     //Quema enemigos: 10DPS durante 3 segundos
  }
+
+
 public class Batemagico{
  public float bateA = 10f; // ataque
   public float bateD = 1f; // defensa
@@ -24,23 +28,32 @@ public float aumentoV = 10; //+10 de vida
  public bool equipado = false;
  public bool Ibag = true;
 }
+
+
 public class Cimitarraelectrica{
    public float ATK = 10f;
 }
+
+
 public class EspadaBastarda{
    public int ataque = 20;
     public bool equipado = false;
 }
+
+
 public class Espadaenllamas{
    public float EspadaLLamasA = 17f; //ataque
     public float EspadaLLamasD = 3f; //DPS por 5 segundos
     public bool equipado = false;
 }
+
 public class Guantedelosmillamentos{
    public float Ataque = 100f;
     public float defensa = 30f; //se resta
     public float VM = 30; //vida maxima
 }
+
+
 public class Lanzadepenas{
    public float ATK = 10f;
     public bool atravezar = true;
---
+++ Assets/Scripts G/movimiento/TKSL.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TKSL : MonoBehaviour
+
+
+{
+    private controladormove Player;
+    
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Player = GetComponentInParent<controladormove>();
+
+    }
+
+    void OnCollisionEnter2D(Collision2D ColP)
+    {
+        if (ColP.gameObject.tag == "piso")
+        {
+            Player.TKSL = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D ColP)
+    {
+        if (ColP.gameObject.tag == "piso")
+        {
+            Player.TKSL = false;
+        }
+    }
+}
