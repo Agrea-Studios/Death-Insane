47451c7bd65ad764335bb6560e37ece5422aa609
Rodrick324 <garcia08rodrigo@gmail.com>
2020-08-28 14:17:05 -0500

    colita
    Ya junte los scripts de las armaduras y los objetos

---
+++ Assets/Codigos BD/BD Equipo Armaduras/ClasesArmaduras.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClasesArmaduras : MonoBehaviour
+{
+    public class UniformePortero{
+        public float defensa = 4;
+    }
+public class UniformeZicagrea{
+    public float UniformemagicD = 2f; // defensa 
+    public float UniformemagicA = 1f; //Ataque
+    public float UniformemagicF = 10f; //Flow
+}
+   
+}
---
+++ Assets/Codigos BD/BD Objetos/ClasesObjetos.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClasesObjetos : MonoBehaviour
+{
+    public class Pmamadoc{
+    public int ATKM = 5;
+    public bool equipado = false;
+    public bool tomado = false;
+    }
+    public class PVivaLavida{
+        public float aumento = 5f;// Regeneracion 
+    public bool tomar = false;
+    public bool equipado = false;
+    }
+}
