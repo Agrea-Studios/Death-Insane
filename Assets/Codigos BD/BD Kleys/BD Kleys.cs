b1e6778a901d5dd20a2faf91be21fd8a0af82263
ricajrgo <61771421+ricajrgo@users.noreply.github.com>
2020-09-01 21:53:49 -0500

    BD Kleys
    Creacion de objetos en la BD KLEYS
    (objetos,armas,etc)

--- Assets/Codigos BD/BD Kleys/BD Kleys.cs
+++ Assets/Codigos BD/BD Kleys/BD Kleys.cs
@@ -10,32 +10,44 @@ public class BDKleys : MonoBehaviour
      public GameObject Clasesxd; 
     //ARMAS Rodrigo
     public ClasesA.Espadabendita espadabenditaxd;
-    public ClasesA.Espadabastardamillamentos xd;
+    public ClasesA.Espadabastardamillamentos espadabastarda1kxd;
+    public ClasesA.Batemagico batemagico;
+    public ClasesA.Cimitarraelectrica cimitarraelectrik;
+    public ClasesA.EspadaBastarda espadabast;
+    public ClasesA.Espadaenllamas espadaonfire;
+    public ClasesA.Guantedelosmillamentos guantede1k;
+    public ClasesA.Lanzadepenas lanzapenaxd;
   
+    // ARMADURAS
+    public ClasesArmaduras.UniformePortero uniformeporteroxd;
+    public ClasesArmaduras.UniformeZicagrea uniformzicagreaxd;
 
-    void Start()
+    // OBJETOS 
+    public ClasesObjetos.Pmamadoc pmamadocxd;
+    public ClasesObjetos.PVivaLavida vivalavidapotion;
+   
+        
+        void Start()
     {
       Clasesxd = GameObject.Find("clasesxd");
-      espadabenditaxd = Clasesxd.GetComponentInParent<ClasesA.Espadabendita>();
+        //ARMAS
+            espadabenditaxd = Clasesxd.GetComponentInParent<ClasesA.Espadabendita>();
+            espadabastarda1kxd = Clasesxd.GetComponentInParent<ClasesA.Espadabastardamillamentos>();
+            batemagico = Clasesxd.GetComponentInParent<ClasesA.Batemagico>(); 
+            cimitarraelectrik = Clasesxd.GetComponentInParent<ClasesA.Cimitarraelectrica>();
+            espadabast = Clasesxd.GetComponentInParent<ClasesA.EspadaBastarda>();
+            espadaonfire = Clasesxd.GetComponentInParent<ClasesA.Espadaenllamas>();
+            guantede1k = Clasesxd.GetComponentInParent<ClasesA.Guantedelosmillamentos>();       
+            lanzapenaxd = Clasesxd.GetComponentInParent<ClasesA.Lanzadepenas>();
+        // ARMADURAS
+            uniformeporteroxd = Clasesxd.GetComponentInParent<ClasesArmaduras.UniformePortero>(); 
+            uniformzicagreaxd = Clasesxd.GetComponentInParent<ClasesArmaduras.UniformeZicagrea>(); 
+        //OBJETOS
+            pmamadocxd = Clasesxd.GetComponentInParent<ClasesObjetos.Pmamadoc>();
+            vivalavidapotion =Clasesxd.GetComponentInParent<ClasesObjetos.PVivaLavida>();
+
+
     }
 
 
-    
-
-
-
-
-
-
-    
-
-    
-     
-   
-
-
-
-
-
- 
 }
